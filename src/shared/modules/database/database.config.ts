66acf3e0a875071c2b024b21b7a6e57ccc39e48c
Bryan Didur <bryan.didur@bgatecnologia.com>
2020-11-02 19:11:25 -0300

    Updated database config options

--- src/shared/modules/database/database.config.ts
+++ src/shared/modules/database/database.config.ts
@@ -4,10 +4,14 @@ import { validateSchema } from '../../helpers/config.helper';
 
 const schema: Joi.ObjectSchema = Joi.object({
   uri: Joi.string().uri().required(),
+  useCreateIndex: Joi.boolean().required(),
+  useFindAndModify: Joi.boolean().required(),
 });
 
 export default registerAs('database', () => {
   return validateSchema(schema, {
     uri: process.env.DB_URI,
+    useCreateIndex: true,
+    useFindAndModify: false,
   });
 });
